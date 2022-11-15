812feabab769f4cec18d532b215b1449ddfb23cd
AutopsyTurvy <102550358+AutopsyTurvy@users.noreply.github.com>
2022-11-16 00:28:03 +0100

    Javascript building to correct animation

---
+++ script.js
@@ -0,0 +1,27 @@
+var character =
+document.getElementById("character");
+var block = document.getElementById("block");
+function jump(){
+    if(character.classlist != "animate"){
+        character.classList.add("animate");
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500)
+}
+
+
+var checkDead = setInterval(function(){
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft =
+    parseInt(window.getComputedStyle(block).getPropertyValue("leeft"));
+
+    if(blockLeft<20 && blockLeft>0 %%
+        characterTop>130){
+            block.style.animation = "none";
+            block.style.display = "none";
+            alert("You Lose!")
+        }
+
+},10);
